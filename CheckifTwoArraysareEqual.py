1a5c877af1eb925e7b7b675b6651b037bbee1c03
Diksha Bisht <86365209+Diksha-Bisht@users.noreply.github.com>
2024-01-28 11:59:04 +0530

    Create CheckifTwoArraysareEqual.py
    Day 1: Problem 5: Given two arrays A and B of equal size N, the task is to find if given arrays are equal or not. Two arrays are said to be equal if both of them contain same set of elements, arrangements (or permutation) of elements may be different though.
    Note : If there are repetitions, then counts of repeated elements must also be same for two array to be equal.

---
+++ CheckifTwoArraysareEqual.py
@@ -0,0 +1,34 @@
+#Day 1: Problem 5: Given two arrays A and B of equal size N, the task is to find if given arrays are equal or not. Two arrays are said to be equal if both of them contain same set of elements, arrangements (or permutation) of elements may be different though.
+Note : If there are repetitions, then counts of repeated elements must also be same for two array to be equal.
+
+class Solution:
+    #Function to check if two arrays are equal or not.
+    def check(self,A,B,N):
+        A.sort()
+        B.sort()
+        if A == B:
+            return 1
+        else:
+            return 0
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__=='__main__':
+    t=int(input())
+    for tc in range(t):
+        
+        N=int(input())
+        
+        A = [int(x) for x in input().replace('  ',' ').strip().split(' ')]
+        B = [int(x) for x in input().replace('  ',' ').strip().split(' ')]
+        ob=Solution()
+        if ob.check(A,B,N):
+            print(1)
+        else:
+            print(0)
+        
+                
+                
+# } Driver Code Ends
