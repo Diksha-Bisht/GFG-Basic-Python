048edd2ba0ea30f7714663adc8d8ad98ca63ee28
Diksha Bisht <86365209+Diksha-Bisht@users.noreply.github.com>
2024-01-28 11:53:32 +0530

    Update BinarySearch.py

--- BinarySearch.py
+++ BinarySearch.py
@@ -1,4 +1,4 @@
-# DAY-1: Binary Search: Given a sorted array of size N and an integer K, find the position(0-based indexing) at which K is present in the array using binary search.
+# DAY-1: Problem 1: Binary Search: Given a sorted array of size N and an integer K, find the position(0-based indexing) at which K is present in the array using binary search.
 class Solution:	
 	def binarysearch(self, arr, n, k):
 		left=0
